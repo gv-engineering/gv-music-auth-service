3def59bc0b27a13c657728d1f03744e656798586
dedVapUch <efimencko93@gmail.com>
2025-06-26 11:32:44 +0300

    Add function verify_token

--- middleware/auth_middleware.py
+++ middleware/auth_middleware.py
@@ -1,41 +1,55 @@
+from datetime import datetime
 from typing import Annotated
 
 from fastapi import Depends
-from jose import jwt
+from jose import jwt, JWTError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
-from common.constants import SECRET_KEY, ALGORITHM
-from crud.user import user_cruds
+from common.constants import SECRET_KEY, ALGORITHM, PUBLIC_KEY
+from crud.auth_services import user_cruds
 from database.init_bd import get_session
-from execption.custom_error import credentials_error
-from models.user import UserModel
+from execption.custom_error import CredentialsException, InvalidTokenException, TokenJWTExpiredException
+from models.user_models import UserModel
 from routs.user_route import scheme_oauth
-from scheme.user import User
+from scheme.auth_scheme import UserCreate
 
 
-async def get_current_user(token: Annotated[str, Depends(scheme_oauth)],
+async def verify_token(token: str = Depends(scheme_oauth)) -> type(scheme_oauth):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        print(payload.get('expire'))
+        if datetime.fromtimestamp(payload.get('expire')) < datetime.now():
+            raise TokenJWTExpiredException
+        return token
+    except JWTError:
+        raise InvalidTokenException
+
+
+async def get_current_user(token: Annotated[str, Depends(verify_token)],
                            session: AsyncSession = Depends(get_session)) -> dict:
-    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    payload = jwt.decode(token, PUBLIC_KEY, algorithms=[ALGORITHM])
     user_id = payload.get('sub')
     if user_id is None:
-        raise credentials_error
+        raise CredentialsException
     user = await user_cruds.get(session, int(user_id), options=([selectinload(UserModel.role)]))
-    user = User(username=user.username, password='', role=str(user.role.role_name), token_author='')
+    user = UserCreate(username=user.username, password_hashed='', role_id=str(user.role.role_name))
     return user.model_dump()
 
 
-async def get_user_info_from_token(token: Annotated[str, Depends(scheme_oauth)]) -> tuple[str, str]:
-    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+async def get_user_info_from_token(token: Annotated[str, Depends(verify_token)]) -> tuple[str, str]:
+    payload = jwt.decode(token, PUBLIC_KEY, algorithms=[ALGORITHM])
     user_id = payload.get('sub')
     role = payload.get('role')
     if user_id is None:
-        raise credentials_error
+        raise CredentialsException
     return user_id, role
 
-async def require_role(require_role: str):
+
+def require_role(require_role: str):
     def role_checker(user: Annotated[dict, Depends(get_current_user)]):
-        if not user.get('role') is require_role:
-            raise credentials_error
-        return user
+        if user.get('role') == require_role:
+            return user
+        raise CredentialsException
+
     return role_checker
